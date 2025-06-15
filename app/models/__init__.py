7891bc4e0080545060cedc5b1ae5b3b3e0b9d99b
João Gabriel Mendes de Quadros <jgquadros2005@gmail.com>
2025-06-15 20:06:11 -0300

    chore: adicionando o arquivo de modularização das classes modelos

---
+++ app/models/__init__.py
@@ -0,0 +1,16 @@
+# Importações dos models para modularização
+from .fabricante_model import Fabricante
+from .funcionario_model import Funcionario
+from .funcionario_patrimonio_model import Funcionario_Patrimonio
+from .notafiscal_model import NotaFiscal
+from .patrimonio_model import Patrimonio
+from .vendedor_model import Vendedor
+
+__all__ = [
+    'Funcionario',
+    'Fabricante', 
+    'Patrimonio',
+    'Vendedor',
+    'NotaFiscal',
+    'FuncionarioPatrimonio'
+]
