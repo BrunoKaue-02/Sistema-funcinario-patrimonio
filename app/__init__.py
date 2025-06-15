402717dd61111d05be213dc7de90d287b14bb164
João Gabriel Mendes de Quadros <jgquadros2005@gmail.com>
2025-06-15 20:01:50 -0300

    chore: adicionando os import de modelo de funcionario e configurando o banco de dados com dotenv

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,29 @@
 from flask import Flask
-
-app = Flask(__name__)
+from app.database.db import db
+import os 
+from dotenv import load_dotenv
 
 def create_app():
     app = Flask(__name__)
 
+    load_dotenv()
+
+    URL = os.getenv('DATABASE_URL');
+
+    # Configuração do banco 
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    # inicializando o SQlAchemy com o app
+    db.init_app(app)
+
+    # Importação das model
+    from app.models import Funcionario
+
+    # Importação e registro das rotas
     from app.routes.home_routes import home_bp
-    from app.routes.pessoa_routes import pessoa_bp
+    from app.routes.funcionario_routes import funcionario_bp
     app.register_blueprint(home_bp)
-    app.register_blueprint(pessoa_bp)
+    app.register_blueprint(funcionario_bp)
 
     return app
