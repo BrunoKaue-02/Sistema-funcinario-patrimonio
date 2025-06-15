bef858d6a3e71b782081f4ed1ae0813fe327fa42
João Gabriel Mendes de Quadros <jgquadros2005@gmail.com>
2025-06-14 22:07:13 -0300

    chore: importando e registrando o blueprint de pessoa

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,8 @@ def create_app():
     app = Flask(__name__)
 
     from app.routes.home_routes import home_bp
-    
+    from app.routes.pessoa_routes import pessoa_bp
     app.register_blueprint(home_bp)
+    app.register_blueprint(pessoa_bp)
 
     return app
