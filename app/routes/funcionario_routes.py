ffb91f6d73499058dc6f2c19a12e90fbe83b0449
João Gabriel Mendes de Quadros <jgquadros2005@gmail.com>
2025-06-20 19:46:37 -0300

    chore: trazendo os arquivos da branch dev para branch dev-joao-thiago

--- app/routes/funcionario_routes.py
+++ app/routes/funcionario_routes.py
@@ -1,8 +1,10 @@
-from flask import Blueprint, render_template
+from flask import Blueprint
 from app.controller.funcionario_controller import listar_todos
 
 funcionario_bp = Blueprint('funcionario', __name__)
 
 # endpoint para listagem de todos os usuarios da tabela funcionarios
 funcionario_bp.route('/funcionario', methods=['GET'])(listar_todos)
+
+
 
